e15dc93ad7c7773f84e880bf32964c5c88e89277
iarmolla <ivan.armolla@gmail.com>
2023-01-11 11:16:35 -0300

    adding animation to card

--- src/app/components/credit/credit.component.ts
+++ src/app/components/credit/credit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
+import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-credit',
@@ -7,54 +7,38 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class CreditComponent implements OnInit {
 
-   //MASTERCARD 5258 
-  // VISA 4901 4901 4901 4901
+  //MASTERCARD 5258 
+  // VISA 4901 
+  focus:boolean= false
   cardForm: FormGroup;
+  number: Number = 0
   constructor() {
     this.cardForm = new FormGroup({
-      number: new FormControl('52', [
-        Validators.pattern('5[1-5][0-9]{14}$'),
-        Validators.required,
-        Validators.maxLength(16),
-      ]),
-      name: new FormControl('', [
-        Validators.pattern('^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$'),
-        Validators.required,
-      ]),
-      dni: new FormControl('', [
-        Validators.pattern('[0-9]{7,9}'),
-        Validators.required,
-        Validators.maxLength(8),
-        Validators.minLength(8),
-      ]),
-      cvc: new FormControl('', [
-        Validators.pattern('^[0-9]+$'),
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(4),
-      ]),
-      date: new FormControl('01/20', [
-        Validators.pattern('([0-9]{2})/([0-9]{2})'),
-        Validators.required,
-      ]),
+      number: new FormControl('', [ Validators.pattern('5[1-5][0-9]{14}$'), Validators.required, Validators.maxLength(16), ]),
+      name: new FormControl('', [ Validators.pattern('^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$'),Validators.required, ]),
+      dni: new FormControl('', [  Validators.pattern('[0-9]{7,9}'), Validators.required,Validators.maxLength(8),Validators.minLength(8),]),
+      cvc: new FormControl('', [ Validators.pattern('^[0-9]+$'),Validators.required,Validators.minLength(3),Validators.maxLength(4),]),
+      date: new FormControl('01/20', [Validators.pattern('([0-9]{2})/([0-9]{2})'),Validators.required,]),
     });
   }
   get cardHolder() {
     return this.cardForm.controls;
   }
   ngOnInit(): void {}
-  onChange(): void {}
+  focused() {  
+    
+    this.focus = !this.focus
+  }
   submitRegistration() {
-    console.log(this.cardForm.controls['name'].value.length);
-    if (this.cardForm.valid) {
-      console.log('formulario enviado!');
+   
+  }
+  
+  inputChange() {
+    if(this.number >=3 && this.number<=4) {
+      console.log('si')
     }
+    this.number = this.cardForm.get('number')?.value
+    console.log(this.number)
   }
-  onChanges(values:any) :void {
-    console.log('entrp')
-
-    console.log(values)
-  }
-  inputChange() {}
 
 }
